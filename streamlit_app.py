5143e3b703363381d5df3cf30ebb2b3ccd44407d
Friendly <friendly@volantis.io>
2024-03-14 01:16:09 +0700

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,21 @@
 import streamlit as st
 from streamlit_player import st_player
 
-url = "https://streetcatpull.hellobike.com/live/4754408094082753545_0.m3u8"
-st_player(url, playing=True, loop=True, controls=True, volume=0.5)
-st.text(url)
+DEFAULT_FEEDER = "4754408094082753545"
 
+params = st.query_params.to_dict()
+feeder_id = params.get('feeder')
+
+
+if feeder_id is None:
+    feeder_id = DEFAULT_FEEDER
+
+url_base = "https://streetcatpull.hellobike.com/live/{}_{}.m3u8"
+
+for i in range(3):
+    url = url_base.format(feeder_id, i)
+    st_player(url, height=250, playing=True, light=True, controls=True, volume=1,  playback_rate=1, progress_interval=100)
+    st.markdown("[{}]({})".format(url, url))
 
 # input = ffmpeg.input(url)
 # audio = input.audio.filter("aecho", 0.8, 0.9, 1000, 0.3)
